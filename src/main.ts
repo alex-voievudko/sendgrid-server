41cce213f3bff546734902c49fd69bc848d602e9
Alex Voievudko <oleksandrv@zeptolab.com>
2023-05-12 19:47:19 +0200

    Configure origins

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'http://localhost:8000',
+      'http://localhost:9000',
+      'http://localhost:3000',
+      'https://alexvoievudko.com',
+    ],
+    methods: 'GET,POST',
+  });
   await app.listen(3000);
 }
 bootstrap();
